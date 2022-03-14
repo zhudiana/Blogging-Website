21c3cb11bf1b475bb087211b6d4f5901cb9686b8
Yodit Fentaw <yoditfentaw90@gmail.com>
2022-03-14 12:58:14 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Navbar from './Components/Navbar';
+import Home from './Components/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <div className="content">
+        <Home />
+      </div>
+      
+    </div>
+    
+  );
+}
+
+export default App;
---
+++ src/Components/Home.js
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react";
+import BlogList from "./BlogList";
+
+const Home = () => {
+  const [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+
+  useEffect(()=> {
+    setTimeout(()=>{
+    fetch('http://localhost:8000/blogs')
+    .then(res => {
+      return res.json();
+    })
+    .then(data => {
+      setBlogs(data);
+      setIsPending(false);
+    });
+  },[]);
+},10000)
+
+  return (
+    <div className="home">
+        { isPending && <div>Loading...</div>}
+        {blogs && <BlogList blogs={blogs} title="All Blogs"  /> }
+    </div>
+  );
+}
+
+export default Home;
